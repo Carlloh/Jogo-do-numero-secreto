ae3deaa15814a19e5d6eecc76907ec9d37d9d8e6
Carlo Cardoso <cccimi@sapo.pt>
2023-12-08 17:50:30 +0000

    Alterando número limite para 50

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -11,7 +11,7 @@ responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do Número Secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100:');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 50:');
 }
 exibirMensagemInicial();
 
